5b5bc0797fc443b409c89f159ad033a11c7cbe9e
Conner Ow <connerow1115@gmail.com>
2021-08-23 11:33:49 -0500

    26477

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"))
 })
 
-app.listen(8080, () => {
+app.listen(8080, "0.0.0.0", () => {
   console.log("Listening on Port 8080")
 });
